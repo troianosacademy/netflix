81e5ad5e495d2838977e62df50b3f1bac39314fb
Victor Tassinari <victortassinarix@gmail.com>
2019-03-22 20:44:23 -0300

    store session on mongo

--- config/express.js
+++ config/express.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path');
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session);
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
 const routesAdmin = require('../app/routes/admin');
 const routesFrontend = require('../app/routes/frontend');
+const configMongoose = require('./mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -20,13 +23,17 @@ nunjucks.configure(path.join(__dirname, '/../app/views'), {
   express: app
 });
 
+configMongoose.connect();
+
 app.use(cookieParser());
 app.use(session({
   secret: 'keyboard cat',
   cookie: { maxAge: 60000 },
   resave: false,
   saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
 }))
+
 
 app.use(fileUpload({
   useTempFiles: true,
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const startup = require('./config/startup');
 
 const { port, env } = require('./config/vars');
 
-mongoose.connect();
-
 (async () => await startup())();
 
 express.listen(port, () => console.log(`server started on port ${port} (${env})`));
